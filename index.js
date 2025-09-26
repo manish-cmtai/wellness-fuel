97e999568814f0d7c3d550636d2aaf9a45a4e3da
manish-cmtai <manish.bourai@completrix.com>
2025-09-26 18:46:00 +0530

    Refactor API routes and update Vercel configuration for versioning

--- index.js
+++ index.js
@@ -1,33 +1,32 @@
-import express from "express"
-import blogRoute from "./routes/blogRoute.js"
-import productRoute from "./routes/productRoutes.js"
-import userRoute from "./routes/userRoute.js"
-import authRoute from "./routes/authRoute.js"
-import dotenv from "dotenv"
-import dbConnection from "./config/db.js"
+import express from "express";
+import blogRoute from "./routes/blogRoute.js";
+import productRoute from "./routes/productRoutes.js";
+import userRoute from "./routes/userRoute.js";
+import authRoute from "./routes/authRoute.js";
+import dotenv from "dotenv";
+import dbConnection from "./config/db.js";
 
-dotenv.config()
+dotenv.config();
 
-const port=process.env.PORT
+dbConnection();
 
+const port = process.env.PORT;
 
-const app =express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.use("/api/blog",blogRoute)
-app.use("/api/product",productRoute)
-app.use("/api/user",userRoute)
-app.use("/api/auth",authRoute)
+app.use("/v1/blog", blogRoute);
+app.use("/v1/product", productRoute);
+app.use("/v1/user", userRoute);
+app.use("/v1/auth", authRoute);
 
-app.get("/",(req,res)=>{
-    res.send("API is running....")
-})
+app.get("/", (req, res) => {
+  res.send("API is running....");
+});
 
+app.listen(port, () => {
+  console.log("server listen on port: ", port);
+});
 
-app.listen(port,()=>{
-    dbConnection()
-    console.log("server listen on port: ",port); 
-})
-
-export default app
+export default app;
