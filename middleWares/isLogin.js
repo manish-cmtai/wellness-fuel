3049bdcd4358a2f570ae6c20c0337b2708c601ef
manish-cmtai <manish.bourai@completrix.com>
2025-10-04 17:17:17 +0530

    fix

--- middleWares/isLogin.js
+++ middleWares/isLogin.js
@@ -3,34 +3,34 @@ import User from "../models/userModel.js";
 
 
 export const isLogin=async(req,res,next)=>{
-next()
-    // const token=req.cookies.token || req.header('Authorization')?.replace('Bearer ', '');
-    
-    // try {
-    //     if(!token|| token===undefined){
-    //        return res.status(404).json({
-    //             message:"No Token"
-    //         })
-    //     }
-    //     const tokenUser=jwt.verify(token,process.env.JWT_TOKEN)
-    //     if(!tokenUser){
-    //         res.status(401).json({
-    //             message:"Invalide"
-    //         })
-    //     }
-    //     const user=await User.findOne({_id:tokenUser.id})
-    //     if(!user){
-    //         res.status(401).json({
-    //             message:"Not Found"
-    //         })
-    //     }
-        
-    //     req.user=user
-    //     next()
 
-    // } catch (error) {
-    //     consoleManager.log(error);
-    //     res.status(500).clearCookie("token").json({message:"islogin error"})
-    // }
+    const token=req.cookies.token || req.header('Authorization')?.replace('Bearer ', '');
+    
+    try {
+        if(!token|| token===undefined){
+           return res.status(404).json({
+                message:"No Token"
+            })
+        }
+        const tokenUser=jwt.verify(token,process.env.JWT_TOKEN)
+        if(!tokenUser){
+            res.status(401).json({
+                message:"Invalide"
+            })
+        }
+        const user=await User.findOne({_id:tokenUser.id})
+        if(!user){
+            res.status(401).json({
+                message:"Not Found"
+            })
+        }
+        
+        req.user=user
+        next()
+
+    } catch (error) {
+        consoleManager.log(error);
+        res.status(500).clearCookie("token").json({message:"islogin error"})
+    }
 
 }
